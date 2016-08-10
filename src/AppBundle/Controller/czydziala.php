c864781a63a77b0339c4a010204fb3d8f76f1e55
ZMASLO <sylwaker@gmail.com>
2016-08-10 11:29:12 +0200

    dodano renderowanie twigiem

---
+++ src/AppBundle/Controller/czydziala.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sylwa
+ * Date: 10.08.2016
+ * Time: 10:45
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
+
+class czydziala extends Controllerr
+{
+    /**
+     * @Route("/{imie}")
+     */
+    public function showAction($imie){
+        $templating = $this->container->get('templating');
+        $html = $templating->render('wyglad/homepage.html.twig', [
+
+            'name' => $imie
+        ])
+
+
+        return new Response($html);
+    }
+}

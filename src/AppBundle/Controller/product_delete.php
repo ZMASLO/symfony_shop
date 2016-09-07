7f43371f970dd0b029b141e6c42bf51408e81edf
ZMASLO <sylwaker@gmail.com>
2016-09-07 14:07:40 +0200

    działa usuwanie produktu

--- src/AppBundle/Controller/product_delete.php
+++ src/AppBundle/Controller/product_delete.php
@@ -7,9 +7,21 @@
  */
 
 namespace AppBundle\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Request;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
-
-class product_delete
+class product_delete extends Controller
 {
+    /**
+     * @Route("product_delete/{id}", name="product_delete")
+     */
+    public function product_delete($id){
+        $em = $this->getDoctrine()->getManager();
+        $product = $em->getRepository('AppBundle:Product')->find($id);
+        $em->remove($product);
+        $em->flush();
+        return $this->redirectToRoute('products');
+    }
 
 }

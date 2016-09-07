033b9c25fd7635d4d738a91b577fbd687e3d94a7
ZMASLO <sylwaker@gmail.com>
2016-09-07 11:03:35 +0200

    Rozpoczynam pracę nad domyślną stroną produktu

--- src/AppBundle/Controller/product_single.php
+++ src/AppBundle/Controller/product_single.php
@@ -7,9 +7,20 @@
  */
 
 namespace AppBundle\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
-
-class product_single
+class product_single extends Controller
 {
+    /**
+     * @param $id
+     * @Route("/products/{id}")
+     */
+    public function showProduct($id){
+        return $this->render('');
+    }
+
+
 
 }
